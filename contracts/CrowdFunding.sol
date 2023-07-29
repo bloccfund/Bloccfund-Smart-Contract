c40bb524350dc23db3580a02f014caf5855389fa
Adeyemi Oluwagbogo <israeladeyemi489@gmail.com>
2023-07-29 13:34:38 +0100

    code formmatting

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
@@ -8,7 +9,9 @@ error CrowdFunding__DonationNotSuccessFully();
 error CrowdFunding__PleaseSendEnough();
 error CrowdFunding__NotOwner();
 error CrowdFunding__DeadlinePassed();
-error CrowdFunding__WithdrawInvestmentFailed();
+error CrowdFunding__Withdrawal();
+error CrowdFunding__YouCannotWithdrawYet();
+error CrowdFunding__CampaignDoesNotExist();
 
 contract CrowdFunding is ReentrancyGuard {
     struct Campaign {
@@ -25,11 +28,6 @@ contract CrowdFunding is ReentrancyGuard {
         uint256[] donations;
     }
 
-    mapping(uint256 => Campaign) public campaigns;
-    mapping(address => mapping (uint256 => uint256)) investorInvestmentinEachCampaign;
-
-    uint256 public campaignId = 0;
-
     event CampaignCreated(
         address indexed _owner,
         string _title,
@@ -39,7 +37,24 @@ contract CrowdFunding is ReentrancyGuard {
         string _image
     );
 
-    modifier isOwner(address owner, uint256 _id) {
+    /////////////
+    // Mapping //
+    /////////////
+
+    mapping(uint256 => Campaign) public campaigns;
+    mapping(address => mapping(uint256 => uint256)) investorInvestmentinEachCampaign;
+
+    uint256 public campaignId = 0;
+
+    ///////////////
+    // Modifiers //
+    ///////////////
+
+    /**
+     * @param _id: Campaign ID
+     */
+
+    modifier isOwner(uint256 _id) {
         Campaign storage campaign = campaigns[_id];
         if (campaign.owner != msg.sender) {
             revert CrowdFunding__NotOwner();
@@ -62,6 +77,27 @@ contract CrowdFunding is ReentrancyGuard {
             }
         }
     }
+    modifier doesCampaignExist(uint256 _id){
+        Campaign storage campaign = campaigns[_id];
+        if(campaign.deadline == 0){
+            revert CrowdFunding__CampaignDoesNotExist();
+        }
+        _;
+    }
+
+    ////////////////////
+    // Main Functions //
+    ////////////////////
+
+    /**
+     * @notice Function for creating a new Campaign
+     * @param _title: Campaign Title
+     * @param _description: Campaign Description
+     * @param _deck: Campaign Pitch Deck
+     * @param _target: Campaign Target
+     * @param _deadline: Campaign Deadline
+     * @param _image: Campaign Image
+     */
 
     function createCampaign(
         string memory _title,
@@ -70,7 +106,7 @@ contract CrowdFunding is ReentrancyGuard {
         uint256 _target,
         uint256 _deadline,
         string memory _image
-    ) external nonReentrant {
+    ) external {
         Campaign storage campaign = campaigns[campaignId];
 
         if (_deadline <= block.timestamp) {
@@ -97,9 +133,13 @@ contract CrowdFunding is ReentrancyGuard {
         );
     }
 
+    /**
+     * @param _id: Campaign ID
+     */
+
     function donateToCampaign(
         uint256 _id
-    ) external payable isDeadlinePassed(_id) {
+    ) external payable isDeadlinePassed(_id) doesCampaignExist(_id) {
         if (msg.value <= 0) {
             revert CrowdFunding__PleaseSendEnough();
         }
@@ -112,27 +152,61 @@ contract CrowdFunding is ReentrancyGuard {
         campaign.amountCollected = campaign.amountCollected + amount;
     }
 
+    /**
+     * @param _id: Campaign ID
+     */
+
     function changeVisibility(
         uint256 _id
-    ) external isOwner(msg.sender, _id) isDeadlinePassed(_id) {
+    ) external isOwner(_id) isDeadlinePassed(_id) doesCampaignExist(_id) {
         Campaign storage campaign = campaigns[_id];
         campaign.onMarketplace = !campaign.onMarketplace;
     }
 
-    function withdrawInvestment(uint256 _id) external isDeadlinePassed(_id) isAnInvestor(_id) {
+    /**
+     * @notice The Function is for investors who wants to withdraw their
+     * investment away from a campaign they are no longer interested in
+     * @param _id: Campaign ID
+     */
+
+    function withdrawInvestment(
+        uint256 _id
+    ) external nonReentrant isDeadlinePassed(_id) isAnInvestor(_id) doesCampaignExist(_id) {
         Campaign storage campaign = campaigns[_id];
-        uint256 amountInvested = investorInvestmentinEachCampaign[msg.sender][_id];
+        uint256 amountInvested = investorInvestmentinEachCampaign[msg.sender][
+            _id
+        ];
         campaign.amountCollected -= amountInvested;
-        (bool success,) = payable(msg.sender).call{value: amountInvested}("");
-        if(!success){
-            revert CrowdFunding__WithdrawInvestmentFailed();
+        (bool success, ) = payable(msg.sender).call{value: amountInvested}("");
+        if (!success) {
+            revert CrowdFunding__Withdrawal();
         }
     }
 
+    /**
+     * @notice This Function is for campaign owners to withdraw the funds they have raised
+     * @notice Withdrawal can not be placed if campaign is not over
+     * @param _id: Campaign ID
+     */
 
+    function withdrawCampaignFunds(uint256 _id) external nonReentrant isOwner(_id) doesCampaignExist(_id) {
+        Campaign storage campaign = campaigns[_id];
+        uint256 amount = campaign.amountCollected;
+        if (campaign.deadline > block.timestamp) {
+            revert CrowdFunding__YouCannotWithdrawYet();
+        } else {
+            (bool success, ) = payable(msg.sender).call{value: amount}("");
+            if (!success) {
+                revert CrowdFunding__Withdrawal();
+            }
+        }
+    }
 
+    //////////////////////
+    // Getter Functions //
+    //////////////////////
 
-    function getInvestors(
+    function getAllInvestors(
         uint256 _id
     ) public view returns (address[] memory, uint256[] memory) {
         return (campaigns[_id].investors, campaigns[_id].donations);
@@ -149,3 +223,6 @@ contract CrowdFunding is ReentrancyGuard {
         return allCampaigns;
     }
 }
+
+// check if camapign exist
+// return all the campaign an investor have invested in
